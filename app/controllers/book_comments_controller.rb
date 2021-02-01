0197ce2200650e1bcb67bc5f0603eb13edf40737
masutaniR <popohu87@gmail.com>
2021-02-01 09:15:04 +0000

    [Finish] 課題3

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,10 +1,12 @@
 class BookCommentsController < ApplicationController
+  before_action :ensure_correct_user, only: [:destroy]
+
   def create
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     if comment.save
-      redirect_back(fallback_location: book_path(book))
+      redirect_to book_path(book)
     else
       @book = Book.find(params[:book_id])
       @book_new = Book.new
@@ -14,12 +16,20 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
+    book = Book.find(params[:book_id])
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_back(fallback_location: book_path(params[:book_id]))
+    redirect_to book_path(book)
   end
 
   private
     def book_comment_params
       params.require(:book_comment).permit(:comment)
+    end
+
+    def ensure_correct_user
+      comment = BookComment.find_by(id: params[:id], book_id: params[:book_id])
+      unless comment.user == current_user
+        redirect_to books_path
+      end
     end
 end
