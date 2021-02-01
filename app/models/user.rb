8d67c8cc43212266082d26902ee119b70108a16a
masutaniR <popohu87@gmail.com>
2021-02-01 10:10:47 +0000

    [Start] 課題4

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,29 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image, destroy: false
 
-  validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
+  validates :name, length: {maximum: 20, minimum: 2},
+                   uniqueness: true
   validates :introduction, length: { maximum: 50 }
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    member do
+      get :following, :followers
+    end
+  end
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
   root 'homes#top'
   get 'home/about' => 'homes#about'
+  resources :relationships, only: [:create, :destroy]
 end
