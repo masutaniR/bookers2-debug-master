ad4b757755ee5596c103b2e45d74d88fa84d02ad
masutaniR <popohu87@gmail.com>
2021-02-02 00:15:05 +0000

    [Add] follows/followers view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def following
+    @title = "Follows"
+    user = User.find(params[:id])
+    @users = user.following.all
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    user = User.find(params[:id])
+    @users = user.followers.all
+    render 'show_follow'
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
