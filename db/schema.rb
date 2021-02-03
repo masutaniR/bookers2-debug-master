9fcbccef9cabeeb150d3684c2453e0dc1df1b320
masutaniR <popohu87@gmail.com>
2021-02-03 00:28:05 +0000

    [Start] 課題7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_093105) do
+ActiveRecord::Schema.define(version: 2021_02_03_002500) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -45,19 +45,7 @@ ActiveRecord::Schema.define(version: 2021_02_01_093105) do
     t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "name"
-    t.text "introduction"
-    t.string "profile_image_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'srting' for column 'address_street'
 
 end
