ef26cd34605a6af028da8af930021fc05034247d
masutaniR <popohu87@gmail.com>
2021-02-08 05:46:39 +0000

    [Update] フォロー非同期

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,14 +1,12 @@
 class RelationshipsController < ApplicationController
 
   def create
-    user = User.find(params[:followed_id])
-    current_user.follow(user)
-    redirect_back(fallback_location: current_user)
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
   end
 
   def destroy
-    user = Relationship.find(params[:id]).followed
-    current_user.unfollow(user)
-    redirect_back(fallback_location: current_user)
+    @user = Relationship.find(params[:id]).followed
+    current_user.unfollow(@user)
   end
 end
